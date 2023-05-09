949ff3197055ac8e06a71f6aa0f8720f07c817ec
ridwan <alfareziridwan@gmail.com>
2023-05-09 17:03:34 +0700

    add: iconify

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -1,9 +1,14 @@
 import Hero from "../Hero";
+import { Icon } from "@iconify/react";
 
 function Home() {
 	return (
 		<>
 			<Hero />
+			<div>
+				<Icon icon="fluent:food-24-filled" className="text-3xl"/>
+				<span>Food</span>
+			</div>
 		</>
 	);
 }
