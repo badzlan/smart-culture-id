ff47bddc4dd53a36d6051f9d09f1a9bb27e8d15e
badzlan <badzlandhabith05@gmail.com>
2023-05-09 15:20:52 +0700

    add: add hero section at homepage

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,14 +1,18 @@
 function Home() {
-	return (
-		<div>
-			<h1 className="text-9xl">Hello World</h1>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis illum
-				corporis, asperiores cupiditate rem nobis hic quae ipsa tempore
-				praesentium?
-			</p>
-		</div>
-	);
+   return (
+      <div className="container my-20">
+         <h1 className="text-7xl mb-10 font-bold">
+            Selamat datang di
+            <span className="text-blue-700 block">Smart Culture ID</span>
+         </h1>
+         <p className="text-xl mb-10 text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, nobis ab dolor ipsum accusantium ducimus voluptas fugiat eos aliquam fugit, provident consectetur omnis in explicabo tenetur pariatur minima, dignissimos dolorum.
+         </p>
+         <a href="#" className="bg-slate-900 text-white px-5 py-4 text-lg rounded-lg tracking-wider font-semibold hover:shadow hover:shadow-md hover:shadow-slate-500">
+            Ayo Eksplor
+         </a>
+      </div>
+   );
 }
 
 export default Home;
